f13e25e5317b862bbc32eba4212d29721490eb97
ndotlara <natalialara.cd@gmail.com>
2017-08-01 16:36:29 -0400

    Updated pseudocode within index.js file
    For planning purposes

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,7 +13,16 @@ $(() => {
 // use require without a reference to ensure a file is bundled
 require('./example')
 
-
+//create an `X` and an `O` marker
 const playerOne = "X"
-const playerTwo = "0"
+const playerTwo = "O"
 // const currentPlayer = // do I need?
+
+//create an event click-handler that places an X where clicked,
+// then, toggles to place an O in new box that is clicked - changing players
+// Create a function where if X=true or O=true, cannot click to replace marker
+// set up an event listener that identifies when a box is clicked
+// reset game board to play new game
+
+//what's the winning game logic/code?
+// what's the tie-game logic/code?
